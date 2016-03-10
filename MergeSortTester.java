e6e9bf62db2a24b5eabf6599834ae779a29e955b
Jackson Dey <Jackson@Jacksons-MacBook-Pro-2.local>
2016-03-09 21:32:29 -0500

    work in progress

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -26,7 +26,13 @@ public class MergeSortTester
      ******************************/
     public static void main( String[] args ) 
     {
-
+	int[] arr0 = {1,3,5,6,8,9,34,45,12,2345};
+	int[] arr1 = new int[100];
+	for(int x = 0; x < 10; x++){
+	    arr1[x] =(Math.random() * 100);
+	}
+	    
+	    
     }//end main
 
 }//end class
